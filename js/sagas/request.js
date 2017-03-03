57a5f2757a1d21a5e31b72ab68b943e1cbc906c7
Ivan Savchenko <ivan.savchenko@eleks.com>
2017-03-03 12:14:53 +0200

    Request updates

--- js/sagas/request.js
+++ js/sagas/request.js
@@ -1,10 +1,25 @@
 import { call, put } from 'redux-saga/effects';
 
-export default function* request(api) {
+export {
+  request as default
+};
+
+const errorHandler = {
+  '401': error => {
+    put({type: 'AUTH_FAILED', error});
+  },
+  '404': error => {
+    put({type: 'NOT_FOUND', error});
+  }
+};
+
+function* request(api) {
   try {
     return yield call(api);
   } catch (error) {
-    yield put({type: 'REQUEST_ERROR', error});
+    if (errorHandler[error.status]) {
+      yield errorHandler[error.status](error);
+    }
     throw error;
   }
 }
